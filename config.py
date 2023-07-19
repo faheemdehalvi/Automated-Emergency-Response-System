bfd67896d5c0b9ad199e558b87a813d478f49755
Vivek Kumar <60113444+vivekkumar7089@users.noreply.github.com>
2023-07-19 13:33:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Twilio credentials
-account_sid = 'AC3690cfc75805fa4612b51ff014990f46'
-auth_token = '7baf09575facbb53fba7ec899b92e978'
-FROM_NUM = +15735945311
+account_sid = 'YOUR_TWILIO_SID' 
+auth_token = 'YOUR_TWILIO_AUTH_TOKEN'
+FROM_NUM = YOUR_TWILIO_PHONE_NUMBER
